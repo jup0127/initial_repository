4356b4b515ae4c988ffb0f56db5a129d66a0bb66
jeon <jup0127@gmail.com>
2014-04-09 16:01:14 +0900

    committed!

--- MyHelloWorld/src/kr/ac/kookmin/cs/myhello/HelloWorld.java
+++ MyHelloWorld/src/kr/ac/kookmin/cs/myhello/HelloWorld.java
@@ -7,7 +7,11 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello World!!");
+			System.out.println("Hello World!!!!");
+	}
+	
+	public void newFunction(){
+		System.out.println("It's New!!");
 	}
 
 }
