2ba4b34292d04926c024b349476e52c112d31aca
hi <jup0127@naver.com>
2014-04-23 19:39:32 +0900

    Revert "good???revert"
    This reverts commit 0fa9de62edd54cec3049c4e5967404daf0883c72.

--- MyHelloWorld/src/kr/ac/kookmin/cs/myhello/HelloWorld.java
+++ MyHelloWorld/src/kr/ac/kookmin/cs/myhello/HelloWorld.java
@@ -2,17 +2,9 @@ package kr.ac.kookmin.cs.myhello;
 
 public class HelloWorld {
 
-
-
-
-
 	/**
 	 * @param args
 	 */
-
-
-
-
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
