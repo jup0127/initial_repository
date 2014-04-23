77ba0e3871c9d8bd740dcc68792a438722085eb1
hi <jup0127@naver.com>
2014-04-23 19:34:31 +0900

    normal commit

--- MyHelloWorld/src/kr/ac/kookmin/cs/myhello/HelloWorld.java
+++ MyHelloWorld/src/kr/ac/kookmin/cs/myhello/HelloWorld.java
@@ -6,9 +6,10 @@ public class HelloWorld {
 	 * @param args
 	 */
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello World!!!!!!!!!!!!");
+			System.out.println("Hello World153462466");
 	}
 
 
